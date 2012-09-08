9e3bf155ecdd43ac5f86adf4bfb5f6d19e2ed60e
Julian Tescher <virulent@gmail.com>
2012-09-08 14:53:13 -0700

    rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquareMeals::Application.config.secret_token = 'db4b1bf7561d09e96ca74d10855b4976b11b776b7f71139135fbc86c15bf576d2906b817b09a6ff76d514ed31ee3ac5fa25987d82e6805eadd5b2a1bbb05bf79'
