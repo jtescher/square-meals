aad0c6dbdc43365277396cf98045c290b86530ca
Matt K <matkam@gmail.com>
2012-09-08 18:43:59 -0700

    Initial locu

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,4 +10,24 @@ class RestaurantsController < ApplicationController
     p url
     respond_with HTTParty.get(url)
   end
+
+  def show
+    @name = params[:name]
+    @lat = params[:lat]
+    @lng = params[:lng]
+    
+    @venue-id = venue_id name, lat, lng
+  end
+
+  private 
+
+  def venue_id(name, lat, lng)
+    url = "http://api.locu.com/v1_0/venue/search/?" +
+            "name=#{name}" +
+            "&location=#{lat},#{lng}"
+
+    json_response = HTTParty.get(url)
+
+    json_response['objects'][0]['id']
+  end
 end
